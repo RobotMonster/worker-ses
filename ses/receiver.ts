68f613d2752124c06827fd8ca911ceded014f1a3
Jo du Plessis <jo@joduplessis.com>
2020-10-01 12:20:48 +0200

    Debugging stuff

--- ses/receiver.ts
+++ ses/receiver.ts
@@ -1,16 +1,29 @@
 const receiver: any = async (event: any, context: any, callback: any): Promise<any> => {
-  // console.log('SESHandler Invoked - v1');
-  //const endpoint: any = new AWS.Endpoint(process.env.AWS_S3_ENDPOINT)
+  console.log('SESHandler Invoked - v1', context);
+
+  // const endpoint: any = new AWS.Endpoint(process.env.AWS_S3_ENDPOINT)
   // Get the mail event
   const mail = event.Records[0].ses.mail;
+  const {
+    timestamp,
+    source,
+    headers,
+  } = mail;
+  const contentId = headers.filter(header => header.name.toUpperCase() == "CONTENT-ID")[0];
+  const inReplyTo = headers.filter(header => header.name.toUpperCase() == "IN-REPLY-TO")[0];
+  const references = headers.filter(header => header.name.toUpperCase() == "REFERENCES")[0];
+  const messageId = headers.filter(header => header.name.toUpperCase() == "MESSAGE-ID")[0];
 
-  console.log('context', context);
-  console.log('mail', mail);
-
-  const { timestamp, source, messageId } = mail;
+  //const originalReference = !references ? null : references.value.split(" ")[0]
   const { from, date, to, subject } = mail.commonHeaders;
 
   console.log({
+    //originalReference,
+    messageId,
+    inReplyTo,
+    contentId,
+    references,
+    messageIdNotHeader: mail.messageId,
     from: from[0],
     to: to[0],
     subject,
